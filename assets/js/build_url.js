58800479dc73c1d564752aa97b436b90da14c207
morgen <xingyuncp@protonmail.com>
2026-03-14 06:19:07 +0800

    add qr code generation feature

--- assets/js/build_url.js
+++ assets/js/build_url.js
@@ -1,10 +1,27 @@
 function build_url(){
-    var url = encodeURI(decodeURI(document.querySelector('#url').value));
+    var urlValue = document.querySelector('#url').value;
+    var url = encodeURI(decodeURI(urlValue));
     if (url === "" || url.indexOf("http") === -1){
         document.getElementById("b_url").innerHTML=`输入的不是链接或者未加http请求头！`;
+        document.getElementById("qrcode").innerHTML="";
     }
     else {
-        url = document.location.href + "api/?url=" + url;
-        document.getElementById("b_url").innerHTML=`<a href=${url} target='_blank'>${url}</a>`;
+        var baseUrl = window.location.protocol + "//" + window.location.host + window.location.pathname;
+        if (!baseUrl.endsWith('/')) {
+            baseUrl = baseUrl.substring(0, baseUrl.lastIndexOf('/') + 1);
+        }
+        var finalUrl = baseUrl + "api/?url=" + url;
+        document.getElementById("b_url").innerHTML=`<a href=${finalUrl} target='_blank'>${finalUrl}</a>`;
+        
+        // 生成二维码
+        document.getElementById("qrcode").innerHTML="";
+        new QRCode(document.getElementById("qrcode"), {
+            text: finalUrl,
+            width: 128,
+            height: 128,
+            colorDark : "#000000",
+            colorLight : "#ffffff",
+            correctLevel : QRCode.CorrectLevel.H
+        });
     }
 }
